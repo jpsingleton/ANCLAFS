8b8241d3f582543129505fdea22a6267ee32c8e4
James Singleton <jpsingleton@users.noreply.github.com>
2017-07-29 14:33:44 +0100

    Some C# 7.1 code

--- Empty.cs
+++ Empty.cs
@@ -1,1 +1,14 @@
+using System;
 
+namespace Locking
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await Task.Delay(100);
+            Console.WriteLine("C# 7.1!");
+            Console.ReadKey(true);
+        }
+    }
+}
